59c8b78e3b61db5f9a75197c92a8d640ee3ccabd
AlokDebnath <AlokDebnath@github.com>
2018-10-22 22:37:44 +0530

    Dictionary based almost complete :)

--- code/dictionary_based.py
+++ code/dictionary_based.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 def get_words():
 	BNC = open("../data/BNC_freq.txt", "r")
@@ -12,4 +13,47 @@ def get_words():
 		index += 1
 	return words
 
-print(get_words())
+def get_corpus():
+	file = open("../data/data.txt", "r")
+	data = file.readlines()
+	information = dict()
+	count = 0
+	key = 0
+	while count < len(data):
+		if count % 4 == 0:
+			key = data[count].split("\t")[1].split("\n")[0]
+		elif count % 4 == 1:
+			information[key] = data[count].split("\t")[1].split("\n")[0]
+		count += 1
+	return information
+
+def classifier(lang1_dict, sentence):
+	regex = re.compile('[@]*[#]*[A-Z]{2,}(?![a-z])[_]*|[@]*[#]*[A-Z][a-z]+(?=[A-Z])[_]*|[@]*[#]*[\w]+[_]*')
+	words = re.findall(regex, sentence)
+	numbers = [str(i) for i in range(1000000)]
+	classed_sent = list()
+	for word in words:
+		if word.lower() in lang1_dict:
+			classed_sent.append((word, "en"))
+		elif '#' in word or '@' in word or '_' in word:
+			classed_sent.append((word, "rest"))
+		elif word in numbers:
+			classed_sent.append((word, "rest"))
+		else:
+			classed_sent.append((word, "hi"))
+
+	return classed_sent
+
+def output(classed_sent, corpus):
+	f = open("../output/dict_output.txt")
+	
+
+# print(get_words()
+# print(get_corpus())
+example = 'My sis @cool_nikkki is saying that "Aaj jo Indian Idol ke Grand Finale me aayi to wo Kareena nhi balki uski hamshakl thi.. " #Irony'
+
+words = get_words()
+corpus = get_corpus()
+classed_sent = classifier()
+for entry in corpus:
+	print(classifier(get_words(), corpus[entry]))
